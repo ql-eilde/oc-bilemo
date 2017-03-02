658f6749697ecb7f78bf54d7997235ba31dadb31
ql-eilde <quentin.leilde@gmail.com>
2017-03-02 10:25:26 +0100

    add fos-restbundle and fos-userbundle / add product and user entity / implement fos-restbundle in the controllers

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -15,6 +15,9 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
+            new JMS\SerializerBundle\JMSSerializerBundle(),
+            new FOS\RestBundle\FOSRestBundle(),
             new AppBundle\AppBundle(),
         ];
 
---
+++ src/AppBundle/Controller/UserController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\UserBundle\Form\Type\RegistrationFormType;
+use FOS\RestBundle\View\View;
+
+class UserController extends Controller
+{
+    /**
+     * @Rest\View(statusCode=Response::HTTP_OK)
+     */
+    public function getUsersAction(){
+        $users = $this->getDoctrine()->getManager()->getRepository('AppBundle:User')->findAll();
+
+        return $users;
+    }
+
+    /**
+     * @Rest\View(statusCode=Response::HTTP_OK)
+     */
+    public function getUserAction($id){
+        $user = $this->getDoctrine()->getManager()->getRepository('AppBundle:User')->find($id);
+
+        if(empty($user)){
+            return View::create(['message' => 'User not found'], Response::HTTP_NOT_FOUND);
+        }
+
+        return $user;
+    }
+
+    /**
+     * @Rest\View(statusCode=Response::HTTP_CREATED)
+     */
+    public function postUsersAction(Request $request){
+        $manip = $this->get('fos_user.util.user_manipulator');
+
+        if(!empty($request->get('username')) && !empty($request->get('email')) && !empty($request->get('password'))){
+            $manip->create($request->get('username'), $request->get('email'), $request->get('password'), 1);
+            return View::create(['message' => 'User created']);
+        } else {
+            return View::create(['message' => 'Something went wrong, please try again'], Response::HTTP_BAD_REQUEST);
+        }
+    }
+}
