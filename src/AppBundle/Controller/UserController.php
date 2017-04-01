1d3f0f2d17c5e63a1517a19f8f942199e405fbd1
Quentin L'eilde <me@quentinleilde.com>
2017-04-01 12:03:52 +0200

    put postUsersAction into a service

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -2,11 +2,6 @@
 
 namespace AppBundle\Controller;
 
-use FOS\UserBundle\Event\GetResponseUserEvent;
-use FOS\UserBundle\FOSUserEvents;
-use FOS\UserBundle\Event\FormEvent;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use FOS\UserBundle\Event\FilterUserResponseEvent;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -86,59 +81,6 @@ class UserController extends Controller
      */
     public function postUsersAction(Request $request)
     {
-        //TODO : Put this in a service
-
-        /** @var $formFactory \FOS\UserBundle\Form\Factory\FactoryInterface */
-        $formFactory = $this->get('fos_user.registration.form.factory');
-        /** @var $userManager \FOS\UserBundle\Model\UserManagerInterface */
-        $userManager = $this->get('fos_user.user_manager');
-        /** @var $dispatcher \Symfony\Component\EventDispatcher\EventDispatcherInterface */
-        $dispatcher = $this->get('event_dispatcher');
-
-        $user = $userManager->createUser();
-        $user->setEnabled(true);
-
-        $event = new GetResponseUserEvent($user, $request);
-        $dispatcher->dispatch(FOSUserEvents::REGISTRATION_INITIALIZE, $event);
-
-        if (null !== $event->getResponse()) {
-            return $event->getResponse();
-        }
-
-        $form = $formFactory->createForm();
-
-        $form->setData($user);
-        $form->submit($request->request->all());
-
-        if (!$form->isValid()) {
-
-            $event = new FormEvent($form, $request);
-
-            $dispatcher->dispatch(FOSUserEvents::REGISTRATION_FAILURE, $event);
-
-            if (null !== $response = $event->getResponse()) {
-                return $response;
-            }
-
-            return $form;
-        }
-
-        $event = new FormEvent($form, $request);
-        $dispatcher->dispatch(FOSUserEvents::REGISTRATION_SUCCESS, $event);
-
-        if ($event->getResponse()) {
-            return $event->getResponse();
-        }
-
-        $userManager->updateUser($user);
-
-        $response = new JsonResponse('User created', JsonResponse::HTTP_CREATED);
-
-        $dispatcher->dispatch(
-            FOSUserEvents::REGISTRATION_COMPLETED,
-            new FilterUserResponseEvent($user, $request, $response)
-        );
-
-        return $response;
+        return $this->get('app.user')->postUser($request);
     }
 }
---
+++ src/AppBundle/User/User.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace AppBundle\User;
+
+use FOS\UserBundle\Event\GetResponseUserEvent;
+use FOS\UserBundle\FOSUserEvents;
+use FOS\UserBundle\Event\FormEvent;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use FOS\UserBundle\Event\FilterUserResponseEvent;
+use FOS\UserBundle\Form\Factory\FormFactory;
+use FOS\UserBundle\Doctrine\UserManager;
+use Symfony\Component\HttpKernel\Debug\TraceableEventDispatcher;
+
+class User
+{
+    private $formFactory;
+    private $userManager;
+    private $dispatcher;
+
+    public function __construct(FormFactory $formFactory, UserManager $userManager, TraceableEventDispatcher $dispatcher)
+    {
+        $this->formFactory = $formFactory;
+        $this->userManager = $userManager;
+        $this->dispatcher = $dispatcher;
+    }
+
+    public function postUser($request)
+    {
+        $user = $this->userManager->createUser();
+        $user->setEnabled(true);
+
+        $event = new GetResponseUserEvent($user, $request);
+        $this->dispatcher->dispatch(FOSUserEvents::REGISTRATION_INITIALIZE, $event);
+
+        if (null !== $event->getResponse()) {
+            return $event->getResponse();
+        }
+
+        $form = $this->formFactory->createForm();
+
+        $form->setData($user);
+        $form->submit($request->request->all());
+
+        if (!$form->isValid()) {
+
+            $event = new FormEvent($form, $request);
+
+            $this->dispatcher->dispatch(FOSUserEvents::REGISTRATION_FAILURE, $event);
+
+            if (null !== $response = $event->getResponse()) {
+                return $response;
+            }
+
+            return $form;
+        }
+
+        $event = new FormEvent($form, $request);
+        $this->dispatcher->dispatch(FOSUserEvents::REGISTRATION_SUCCESS, $event);
+
+        if ($event->getResponse()) {
+            return $event->getResponse();
+        }
+
+        $this->userManager->updateUser($user);
+
+        $response = new JsonResponse('User created', JsonResponse::HTTP_CREATED);
+
+        $this->dispatcher->dispatch(
+            FOSUserEvents::REGISTRATION_COMPLETED,
+            new FilterUserResponseEvent($user, $request, $response)
+        );
+
+        return $response;
+    }
+}
